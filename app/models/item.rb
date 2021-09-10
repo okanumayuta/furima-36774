a7b726ba70587c6b62dc066736d6fda86ee8d35e
okanumayuta <mj.dokinchan@outlook.com>
2021-09-10 17:50:44 +0900

    出品商品保存機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,14 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :catetory
+  belongs_to :category
   belongs_to :sales_status
-  belongs_to :Shipping_fee_status
-  belongs_to :Prefecture
-  belongs_to :Scheduled_delivery
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 
   validates :name, :price, :info, presence: true
   validates :price, format: { with: /\A[0-9]+\z/ }
   validates :price, numericality: { in: 300..9999999 }
-  validates :category_id, :sales_status_id, :Shipping_fee_status_id, :Prefecture_id, :Scheduled_delivery_id, numericality: { other_than: 0 , message: "can't be blank"} 
+  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 0 , message: "can't be blank"} 
 end
