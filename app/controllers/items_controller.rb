c214ba069502a06562c3beb8c8f0b8bd3895897d
okanumayuta <mj.dokinchan@outlook.com>
2021-09-13 21:01:35 +0900

    アクセス制限追加実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -35,11 +34,19 @@ class ItemsController < ApplicationController
     end
   end
 
-end
+  private
 
-private
+  def item_params
+    params.require(:item).permit(:image, :name, :price, :info, :category_id, :sales_status_id, :shipping_fee_status_id,
+                                 :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
+  end
 
-def item_params
-  params.require(:item).permit(:image, :name, :price, :info, :category_id, :sales_status_id, :shipping_fee_status_id,
-                               :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
+  def move_to_index
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
 end
